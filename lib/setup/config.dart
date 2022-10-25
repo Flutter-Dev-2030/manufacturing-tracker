fafc3c008a47862f8209287702244babcd28a522
viannaandrebr <viannaandresouza@gmail.com>
2022-10-25 02:56:46 -0300

    Deploy Blockchain Infura 2

--- lib/setup/config.dart
+++ lib/setup/config.dart
@@ -2,8 +2,13 @@ class Config {
   // static const String _rpcURL = "http://10.0.2.2:7545";
   // static const String _wsURL = "ws://10.0.2.2:7545/";
 
-  static const String _rpcURL = "http://127.0.0.1:7545";
-  static const String _wsURL = "ws://127.0.0.1:7545/";
+  // static const String _rpcURL = "http://127.0.0.1:7545";
+  // static const String _wsURL = "ws://127.0.0.1:7545/";
+
+  static const String _rpcURL =
+      "https://goerli.infura.io/v3/edce2586c06f42bbb7f5ff69c4d092da";
+  static const String _wsURL =
+      "https://goerli.infura.io/v3/edce2586c06f42bbb7f5ff69c4d092da";
 
   static String get rpcURL => _rpcURL;
   static String get wsURL => _wsURL;
